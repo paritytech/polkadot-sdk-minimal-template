7b16ab63a2561bad12f0066348297f4526eeb313
rzadp <roopert7@gmail.com>
2024-06-12 12:12:58 +0200

    debian/ubuntu

--- .github/scripts/ubuntu-prerequisites.sh
+++ .github/scripts/ubuntu-prerequisites.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 apt-get update
-apt-get install -y curl git protobuf-compiler
+apt-get install -y curl git protobuf-compiler build-essential libclang-15-dev
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
